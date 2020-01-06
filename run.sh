15fc8fd366ff420fa9c02a5d2091027c13be99fd
Riaan Nolan <riaan.nolan@servian.com>
2020-01-06 14:27:36 +1100

    testing hence the echo

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 # Import credentials as env variables
 source .env
 
 # Convert markdown to html
 for i in $(find . -name $MARKDOWN_EXTENSION);
-  do mkdir -p html/$(dirname ${i:2}); pandoc $i -f markdown -s -o html/${i:2}.html;
+  do echo mkdir -p html/$(dirname ${i:2}); echo pandoc $i -f markdown -s -o html/${i:2}.html;
 done
 
 # Convert links to svg files to svg data
@@ -14,11 +14,11 @@ python svg2html.py
 # Cleanup:
 # now rename html/*tmp to html
 for i in $(find html -name "*.tmp");
-  do mv $i ${i:0:-4};
+  do echo mv $i ${i:0:-4};
 done
 
 # Upload html/* to confluence
 cd html
 for i in $(find . -name $HTML_EXTENSION);
-  do ../acli.sh confluencecloud -a storePage --file $i --space $CONFLUENCE_SPACE --title ${i:2} -s $CONFLUENCE_WIKI_LINK --user $CONFLUENCE_USER --token $CONFLUENCE_TOKEN --replace --parent $CONFLUENCE_PARENT;
+  do echo ../acli.sh confluencecloud -a storePage --file $i --space $CONFLUENCE_SPACE --title ${i:2} -s $CONFLUENCE_WIKI_LINK --user $CONFLUENCE_USER --token $CONFLUENCE_TOKEN --replace --parent $CONFLUENCE_PARENT;
 done
