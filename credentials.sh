c90cc46f442931566c24fe13c1ab6ddeb926d852
Riaan Nolan <riaan.nolan@servian.com>
2020-01-06 14:15:41 +1100

    changes and fixes

--- credentials.sh
+++
@@ -1,7 +0,0 @@
-MARKDOWN_EXTENSION = "*.md";
-HTML_EXTENSION = "*.html";
-CONFLUENCE_SPACE = "xx";
-CONFLUENCE_WIKI_LINK = "https://vibrato.atlassian.net/wiki";
-CONFLUENCE_USER = "xxx.xxx@vibrato.com.au"
-CONFLUENCE_TOKEN =  "XXXXXXX"
-CONFLUENCE_PARENT = "000"
